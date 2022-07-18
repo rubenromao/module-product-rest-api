eade7497660ffdeb384642f0fffbf004002b7035
Ruben Romao <rubenromao@gmail.com>
2022-07-18 14:28:45 +0100

    Revert "CS"
    This reverts commit 27f9b38b9b588a002d55777e29ecd6e44fe6b250.

--- Test/Integration/_files/enable_oss_rollback.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Rezolve\APISalesV4\Test\FixtureLoaders\ConfigurationLoader;
-
-$ossConfigArray = [
-    ['path' => 'rezolve/cdn/backend', 'value' => 'S3']
-];
-$configLoader   = new ConfigurationLoader($ossConfigArray);
-$configLoader->loadConfigurationForApi();
--- Test/Integration/_files/performance_rollback.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Rezolve\APISalesV4\Test\FixtureLoaders\CategoryLoader;
-
-$largeCategory  = [];
-require __DIR__ . '/_fixtures/large_category.php';
-$categoryLoader = new CategoryLoader($largeCategory);
-$categoryLoader->removeCategories();
